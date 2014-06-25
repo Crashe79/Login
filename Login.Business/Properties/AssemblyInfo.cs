b300c03e1c06d423ff91a7f9e21021eb631af8e0
AAnshakov <alexey.anshakov@gmail.com>
2014-06-25 21:15:11 +0300

    AssemblyInfo has been changed

--- Login.Business/Properties/AssemblyInfo.cs
+++ Login.Business/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Profile.Business")]
+[assembly: AssemblyTitle("Login.Business")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Profile.Business")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("webRunes")]
+[assembly: AssemblyProduct("Login.Business")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
