75063f95e7d79c5825256c9da39f982a9d6d1ab2
Alexey Anshakov <alexey.anshakov@gmail.com>
2014-09-11 10:31:51 +0300

    AssemblyInfos update

--- Login.Services/Properties/AssemblyInfo.cs
+++ Login.Services/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Login.Services")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("webRunes")]
 [assembly: AssemblyProduct("Login.Services")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
