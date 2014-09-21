3fe32ac34d5bf44f946a61829f6b1a7cf7f101c0
Pavel Fokin <2268651@gmail.com>
2014-09-21 10:22:54 +0300

    Release User Account Service

---
+++ UserAccount/Authorization/CustomAutentificationModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Security;
+
+namespace UserAccount.Authorization
+{
+    public class CustomAutentificationModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.AuthenticateRequest += OnAuthenticateRequest;
+        }
+
+        private static void OnAuthenticateRequest(object sender, EventArgs e)
+        {
+            var httpApplication = (HttpApplication)sender;
+            var context = httpApplication.Context;
+            if (context.User != null && context.User.Identity.IsAuthenticated)
+                return;
+
+            var httpCookie = httpApplication.Request.Cookies["webrunes_polity"];
+            if (httpCookie == null)
+                return;
+
+            try
+            {
+                var ticket = FormsAuthentication.Decrypt(httpCookie.Value);
+                var account = Account.Deserialize(ticket.UserData);
+                var customIdentity = new CustomIdentity(account, ticket.Name);
+                var principal = new GenericPrincipal((IIdentity) customIdentity, customIdentity.GetRoles());
+                context.User = principal;
+                Thread.CurrentPrincipal = (IPrincipal) principal;
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ UserAccount/IProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UserAccount.Authorization;
+
+namespace UserAccount
+{
+    public interface IProfile
+    {
+        bool IsAuthenticated { get; }
+        string GetUserAccountGuid();
+        string LoginBySocialNT(RegistrationTokenModel registrationTokenModel);
+        bool LogOut(Guid userGuid);
+        UserPublicProfile GetUserPublicProfile();
+    }
+}
